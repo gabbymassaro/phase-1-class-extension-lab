f23671a1a3fc17afa7912378c52047d2900261fa
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-06 09:27:40 -0400

    Polygon passes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides
+  }
+
+  get countSides() {
+    let count = this.sides.length
+    return count
+  }
+
+  get perimeter() {
+    let sumEach = 0
+    this.sides.forEach((side, index) => {
+        sumEach += side
+    })
+    return sumEach
+  }
+}
+
+let polygon = new Polygon( [ 5, 5, 5 ] )
+console.log(polygon.countSides)
+console.log(polygon.perimeter)
